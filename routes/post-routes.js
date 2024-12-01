96e0811683a3c642976125a31bef1c10be16c4e8
Roni Twito <ronitwito08@gmail.com>
2024-12-01 18:59:24 +0200

    add-controllers

--- routes/post-routes.js
+++ routes/post-routes.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const Post = require('../models/post-model');
-const { createPost, updatePost } = require('../controllers/post-controllers');
+const { createPost, updatePost , deletePost ,getPostById ,getPosts } = require('../controllers/post-controllers');
 
 // Create a new post
 router.post('/create', createPost);
 
 //update post
 router.post('/update/:id' , updatePost);
+
+// Get all posts
+router.get('/posts', getPosts);
+
+// Get post by id
+router.get('/post/:id', getPostById);
+
+// Delete post by id
+router.delete('/delete/:id', deletePost);
 
 module.exports = router;
